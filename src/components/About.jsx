e7afda58ded87aa66687f0c3376183b74785a501
TusharJain2000 <tusharjain01234@gmail.com>
2024-04-13 02:13:45 +0530

    changed height in about, home, Project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,14 +6,14 @@ const About = () => {
       name="about"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
+      <div className="max-w-screen-lg pt-2 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-4">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
         </div>
 
-        <p className="text-xl mt-20">
+        <p className="text-xl mt-4">
           Hi, my name is<span className="font-bold">{" Tushar Jain"}</span> and I am currently a{" "}
           <span className="font-bold">{"3rd Year Student"}</span>
           {" "} studying in{" "}<span className="font-bold">{"Pandit Deendayal Energy University"}</span> pursuing <span className="font-bold">{" Bachelors in Technology in Information and Technology."}</span> 
